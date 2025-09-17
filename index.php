87cdd3f84a6adb663322337bbb6457328eda8132
madddpg <pagutaad@students.nu-lipa.edu.ph>
2025-09-17 14:42:21 +0800

    fix: add shortcut icon link to the HTML head

--- index.php
+++ index.php
@@ -35,6 +35,7 @@ $activePromos = $promoStmt->fetchAll(PDO::FETCH_ASSOC);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="img/logo.png" type="image/png">
     <link rel="stylesheet" href="./css/style.css">
 </head>
 
